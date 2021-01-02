e955e9aa5e969e968ae3bfc386313af471eebb4b
pweber <4fd49ecc792958c1ed7495830435490f@patweber.de>
2021-01-02 05:45:38 +0100

    initiate database, not working
    media static libs not exist

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -4,11 +4,10 @@ envsubst </var/templates/bounca/main.ini >/var/www/bounca/etc/bounca/main.ini
 envsubst </var/templates/bounca/bounca.yaml >/var/www/bounca/etc/bounca/services.yaml
 mkdir -p /run/nginx $PRE_START_PATH /run/uwsgi/app/bounca/
 chown -R www-data:www-data /run/uwsgi/app/bounca/
-set -e
 
 # If there's a prestart.sh script in the /app directory, run it before starting
 find $PRE_START_PATH -prune -path "*.sh" -type f -exec sh -c '"$0"' {} \;
+cd  /var/www/bounca  && source venv/bin/activate && python3 manage.py migrate --noinput && deactivate
 
 # Start Supervisor, with Nginx and uWSGI
-cd /var/www/bounca
 exec /usr/bin/supervisord
